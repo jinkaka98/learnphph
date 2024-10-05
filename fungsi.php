3d43532006188c71235781dde591491f6c69dae7
GULAM ASSAD SIDIQIE <55033771+jinkaka98@users.noreply.github.com>
2024-10-05 15:50:31 +0700

    up

---
+++ fungsi.php
@@ -0,0 +1,23 @@
+<?php
+echo "Hello World!";
+
+echo '<br>';
+
+// fugnsi string
+$a1 = 'Hello World!';
+$a2 = 'Hello World!';
+$a3 = 'Hello, World!';
+$a33 = 'World';
+$a4 = 'Hello World!';
+$a5 = 'Hello World!';
+
+echo strlen($a1); // menghitung panjang string/text
+echo '<br>';
+echo strrev($a2); // membalik string/huruf
+echo '<br>';
+
+
+$x = 5;
+$y = 2;
+echo $x <> $y;
+?>
