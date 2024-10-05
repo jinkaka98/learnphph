ec323735851fc6f2bf590efccc1fbb82c36b9d2f
GULAM ASSAD SIDIQIE <55033771+jinkaka98@users.noreply.github.com>
2024-10-05 07:35:46 +0700

    up

--- index.php
+++ index.php
@@ -49,6 +49,9 @@ baris
 // echo "ayam"."ayam";
 
 // menggabungkan sting atau text di php
+
+use LDAP\Result;
+
 $txt1 = "Tutorial PHP";
 $txt2 = "Assad.com";
 $x = 5;
@@ -110,4 +113,129 @@ echo "Aku Belajar pemrogramana PHP di " . $txt . "<br>";
 $tht = "assad.com";
 printf("Aku Belajar Pemograman di %s<br>", $txt);
 
+echo "<br>";
+
+// misal kita punya bilangan  dengan notasi e seperti ini 
+$harga = 1000000;
+
+// jika kita cetak dnegan echo
+echo "Harga adalah Rp $harga";
+echo "<br>";
+// jika kita cetak dengan printif
+printf("Harganya adalah Rp %.2f", $harga);
+
+echo "<br>";
+// mencetak tanda kutip
+echo 'hari ini hari jum\'at';
+
+echo  "<br>";
+$teman = "andi";
+if($teman == "andi"){
+    echo "dia adalah teman saya";
+}else{
+    echo "dia bukan teman saya";
+}
+
+echo "<br>";
+echo "<br>";
+
+$teman1 = "budi";
+if($teman1 == "budi"){
+    echo "budi adalah teman saya";
+}elseif($teman1 == "andi"){
+    echo "andi adlaah teman saya";
+}else{
+    echo "saya tidak punya teman";
+}
+
+
+
+// tiga variable berbeda
+$Belajar = "";
+$BELAJAR = "";
+$belajar = "";
+echo "<br>";
+// boleh menggunakna underscore
+
+$nama_barang = "Buku PHP";
+$namaPembeli = "Petani Kode"; // <-- menggunakan camelCase
+
+echo "<br>";
+// mengambil nilai variable
+// membuat variabel baru
+$nama_barang = "Minyak Goreng";
+$harga = 15000;
+
+// menampilkan isi variabel
+echo "Ibu membeli $nama_barang seharga Rp $harga";
+
+echo "<br>";
+
+$stok_1 = "ayam goreng";
+$harga_1 = "15."."000";
+
+// print("Sandi membeli $stok_1, dengan harga $harga_1")
+
+
+
+echo "jono Memberli $stok_1, Dengan Harga $harga_1";
+
+echo "<br>";
+
+
+echo "<br>";
+
+//database data diri
+$nama_lengkap = "andika mahesa";
+$jenis_kelamin = "L";
+$umur = "26";
+$berat = "55 Kg";
+$tinggi = "167 cm";
+$menikah = false;
+
+// cetak data
+echo "<h1>Biodata Diri</h1>";
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur<br>";
+echo "Berat Badan: $berat<br>";
+echo "Tinggi Badan: $tinggi<br>";
+echo "Menikah: $menikah<br>";
+
+
+echo "<br>";
+
+//aray lear
+// arai tu ada 2 format
+// $buah = array();
+// $hobi = [];
+// contoh
+$minuman = array("Kopi","Teh","susu","jus jeruk");
+$makanan = ["nasi goyeng","mie telor","ayam kecap"];
+
+// result
+echo "andika mau pesen ". $makanan[0] . " dengan minum " . $minuman[0];
+
+echo "<br>";
+
+$barang = ["Buku Tulis", "Penghapus", "Spidol"];
+
+// menampilkan isi array
+echo $barang[0]."<br>";
+echo $barang[1]."<br>";
+echo $barang[2]."<br>";
+
+var_dump($barang);
+
+
+echo "<br>";
+echo "<br>";
+//kita perbesar aaray nya
+$x_kunci = [
+    "nama" => "Andika",
+    "umur" => "26 Tahun",
+    "Asal Sekolah" => "Smk BPK Penabur",
+];
+
+var_dump($x_kunci);
 ?>
